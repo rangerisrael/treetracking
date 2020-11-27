ce0165e03aebabc823be3376dd2b86c66891b642
rangerisrael <38226544+rangerisrael@users.noreply.github.com>
2020-11-27 16:22:14 +0800

    Update config.php

--- views/map/config.php
+++ views/map/config.php
@@ -3,9 +3,11 @@
 
 return [
 	"db" => [
-		"server" => "localhost",
-		"dbname" => "treetracking",
-		"user" => "root",
-		"pass" => "",
+		"server" => "sql12.freemysqlhosting.net",
+		"dbname" => "sql12378754",
+		"user" => "sql12378754",
+		"pass" => "VB8hK9jQyM",
 	],
+	
+
 ];
