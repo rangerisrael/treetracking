19f87b54594d39d27299ff74a7dba486d3e5ae57
rangerisrael <38226544+rangerisrael@users.noreply.github.com>
2020-11-27 16:13:14 +0800

    Update config.php

--- views/config.php
+++ views/config.php
@@ -1,10 +1,18 @@
 <?php
 date_default_timezone_set("Asia/Manila");
 $date_time = date('Y-m-d h:i:s');
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "treetracking";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "treetracking";
+
+
+$servername = "sql12.freemysqlhosting.net";
+$username = "sql12378754";
+$password = "VB8hK9jQyM";
+$dbname = "sql12378754";
+
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
