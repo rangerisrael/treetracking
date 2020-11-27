fd5ec14c4ed58a882d93fd5449a5fe65d2d8657b
rangerisrael <38226544+rangerisrael@users.noreply.github.com>
2020-11-27 16:15:16 +0800

    Update config.php

--- models/config.php
+++ models/config.php
@@ -1,9 +1,15 @@
 <?php
 
-	$db_host='localhost';
-	$db_user='root';
-	$db_pass='';
-	$db_name='treetracking';
+// 	$db_host='localhost';
+// 	$db_user='root';
+// 	$db_pass='';
+// 	$db_name='treetracking';
+
+	$db_host='sql12.freemysqlhosting.net';
+	$db_user='sql12378754';
+	$db_pass='VB8hK9jQyM';
+	$db_name='sql12378754';
+
 
 	try {
 		$db_conn= new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_pass);
